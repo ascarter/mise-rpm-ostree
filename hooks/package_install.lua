f9d3c27dc8d19535543bab4fe67ebab60c48c6be
Andrew Carter <ascarter@users.noreply.github.com>
2026-09-05 08:40:57 -0700

    feat: install exact package versions

--- hooks/package_install.lua
+++ hooks/package_install.lua
@@ -3,5 +3,5 @@ local rpm_ostree = require("lib.rpm_ostree")
 function PLUGIN:PackageInstall(ctx)
   rpm_ostree.names(ctx.packages)
   rpm_ostree.refresh(ctx)
-  return rpm_ostree.action(ctx, { "rpm-ostree", "install", "--idempotent" }, "package installation")
+  return rpm_ostree.action(ctx, { "rpm-ostree", "install", "--idempotent" }, "package installation", true)
 end
